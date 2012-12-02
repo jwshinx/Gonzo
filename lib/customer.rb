7f565e8032c8069c00d3508a5f03290b48d9d649
Joel Shin <jwshin@gmail.com>
2012-12-02 11:49:34 -0800

    refactored customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,17 +15,13 @@ class Customer
   total_amount, new_release_download_points = 0, 0
   result = "\nDownload Records for #{name}\n"
   @downloads.each do |element|
-   this_amount = amount_for element
-
-   result += "\t" + element.song.title + " " + this_amount.to_s + "\n"
-   total_amount += this_amount
-
    if element.song.price_code == Song::NEW_RELEASE
     new_release_download_points += 1
    end
+
+   result += "\t" +element.song.title+ " " + element.charge.to_s + "\n"
+   total_amount += element.charge 
   end
-  
-  #frequent_downloader_points = @downloads.length-1 
 
   result += "Amount owed is #{total_amount}\n"
   result += "You earned #{new_release_download_points} point(s)"

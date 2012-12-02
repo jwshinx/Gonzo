1d2a7e486050c8f2ad22d0d41b010d5902b48a6b
Joel Shin <jwshin@gmail.com>
2012-12-01 22:02:08 -0800

    more specs

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -7,5 +7,10 @@ describe "Customer" do
   it { customer.name.should == 'Thom Yorke' }
   it { customer.downloads.length.should == 0 }
  end
+ describe "with 1 download" do
+  let(:c) { FactoryGirl.build( :one_download_customer ) }
+  it { c.downloads.length.should == 1 }
+  it { c.downloads.first.title.should == "Stand" }
+ end
 end
 
